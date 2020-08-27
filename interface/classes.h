f5906d0eb10945b7b4a83172dc9d555e4c38f9f6
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-28 02:34:09 +0530

    Update classes.h

--- interface/classes.h
+++ interface/classes.h
@@ -1,3 +1,3 @@
-#include "ProdTutorial/ProducerTest/classes/framePredCollection.h"
+#include "ProdTutorial/E2eDlRecClasses/classes/framePredCollection.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 #include <vector>
