8ecacd990024a49a48fbd244609609d91a171188
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-28 15:56:55 +0530

    Update framePredCollection.cc

--- src/framePredCollection.cc
+++ src/framePredCollection.cc
@@ -1,4 +1,4 @@
-#include "ProdTutorial/E2eDLRecClasses/interface/framePredCollection.h"
+#include "ProdTutorial/E2eDLrecClasses/interface/framePredCollection.h"
 
 framePredCollection::~framePredCollection(){
     frameCollection.clear();
