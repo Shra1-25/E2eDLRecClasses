cb14178f93492bbeeee3ee33fcb6f23ebf1a0b19
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-28 08:16:25 +0530

    Update classes.h

--- src/classes.h
+++ src/classes.h
@@ -1,3 +1,4 @@
 #include "ProdTutorial/E2eDlRecClasses/interface/framePredCollection.h"
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Common/interface/SortedCollection.h"
 #include <vector>
